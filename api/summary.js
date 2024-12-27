f5ecfdfdc5b56a6d3dcaa376c47d49db4e188a31
Evgenii Chkalov <evgeniic@lucanet.com>
2024-12-27 20:15:32 +0100

    added logs

--- api/summary.js
+++ api/summary.js
@@ -11,7 +11,9 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  // Allow headers
 
     const body = req.body;
+    console.log('Body: ', body);
     const prompt = promtGetSummary + body;
     const result = await model.generateContent(prompt);
+    console.log('Response: ', result.response.text());
     res.status(200).json({ message: result.response.text() });
 }
