0d2376007b7ecdb4fb21b44a2c58436dedbe1b5e
Evgenii Chkalov <evgeniic@lucanet.com>
2024-12-27 20:09:39 +0100

    resolve cors issue

--- api/summary.js
+++ api/summary.js
@@ -6,6 +6,10 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const promtGetSummary = 'Я предоставлю тебе сообщения из переписки нескольких пользователей в формате: "Пользователь {username} сказал: {message} \n". Тебе нужно будет выделить 1-3 основные темы, которые обсуждали пользователи, и кратко изложить суть диалога в 1-2 предложениях по каждой теме. Формат ответа может выглядеть так: "[emoji-of-fish] Рыбалка. Пользователь username1 пригласил username2 и username3 на рыбалку, но они отказались. \n [emoji-of-earth] Компьютерные игры. Username1 пригласил username2 поиграть в игру в субботу в 15:00 \n (и так далее)". \n Вот сама переписка, используй эти сообщения: ';
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins (you can change '*' to specific origins)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');  // Allow specific methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  // Allow headers
+
     const body = req.body;
     const prompt = promtGetSummary + body;
     const result = await model.generateContent(prompt);
