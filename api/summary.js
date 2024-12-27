771f3bb227f79ca4e3ca3beef5a93154d310a436
Evgenii Chkalov <evgeniic@lucanet.com>
2024-12-27 20:02:22 +0100

    added summary api

---
+++ api/summary.js
@@ -0,0 +1,13 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const GEMINI_TOKEN = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(GEMINI_TOKEN);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const promtGetSummary = 'Я предоставлю тебе сообщения из переписки нескольких пользователей в формате: "Пользователь {username} сказал: {message} \n". Тебе нужно будет выделить 1-3 основные темы, которые обсуждали пользователи, и кратко изложить суть диалога в 1-2 предложениях по каждой теме. Формат ответа может выглядеть так: "[emoji-of-fish] Рыбалка. Пользователь username1 пригласил username2 и username3 на рыбалку, но они отказались. \n [emoji-of-earth] Компьютерные игры. Username1 пригласил username2 поиграть в игру в субботу в 15:00 \n (и так далее)". \n Вот сама переписка, используй эти сообщения: ';
+export default async function handler(req, res) {
+    const body = req.body;
+    const prompt = promtGetSummary + body;
+    const result = await model.generateContent(prompt);
+    res.status(200).json({ message: result.response.text() });
+}
